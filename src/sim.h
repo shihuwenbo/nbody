d61ffdf5a26a41bfaf1725f1fe64c33e0ad25d17
Huwenbo Shi <shihuwenbo@ucla.edu>
2014-12-20 15:36:46 -0800

    added more videos

--- src/sim.h
+++ src/sim.h
@@ -4,10 +4,10 @@
 #define SIM
 
 // simulation parameters
-const float npart = 500;
-const float screen_size = 640.0;
-const float screen_center = 320.0;
-const float screen_scale = 640;
+const float npart = 1000;
+const float screen_size = 700.0;
+const float screen_center = 350.0;
+const float screen_scale = 700;
 const float mass_scale = 1.0;
 const float delta_t = 0.1;
 const float grav_const = 6.67384e-11;
